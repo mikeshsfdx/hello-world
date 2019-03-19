b29e2a21fe48173d9bc65cff079f59efcf44bb60
Mikesh Maharjan <mikesh.maharjan@laputatech.com>
2019-03-19 17:29:34 +0800

    Add webhooks for nexmo

---
+++ routes/nexmo.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/delivery', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/inbound', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
